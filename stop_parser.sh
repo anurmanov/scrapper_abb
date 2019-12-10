9688735c9f8f43ab4eb05c9558281d4e0ff3cb50
Alisher Nurmanov <alisher.nurmanov@inbox.ru>
2019-12-10 20:31:30 +0600

    fixed stop_parser.sh, editing status_work field

--- stop_parser.sh
+++ stop_parser.sh
@@ -1,1 +1,1 @@
-docker exec abb_mariadb mysql -uscrapper -p123456 allbestbets --init-command="update work_parser set status=4;"
+docker exec abb_mariadb mysql -uscrapper -p123456 allbestbets --init-command="update work_parser set status=4, status_work='parser was stoped...';"
