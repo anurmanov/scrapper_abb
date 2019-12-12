d772fb23c039a17dd95c534cea3d5516f85d7208
Alisher Nurmanov <alisher.nurmanov@inbox.ru>
2019-12-12 16:27:43 +0600

    addde delay before closing loop

--- script.py
+++ script.py
@@ -9,4 +9,6 @@ if __name__ == '__main__':
     #using asynchronous code we can 'concurrently' run multiple parsers of the different bet companies
     #tasks = [loop.create_task(abb.parse_periodic()), loop.create_task(bt1.parse_periodic()), loop.create_task(bt2.parse_periodic()),]
     loop.run_until_complete(asyncio.wait(tasks))
+    #to ensure that underlying aiohttp connections are closed we must add at least zero-sleep delay
+    loop.run_until_complete(asyncio.sleep(0))
     loop.close()
